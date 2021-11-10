bc91da77d154cd0bfe2b3d52eca9d5157f016bfd
iago fagundes <iago.fagundes@outlook.com>
2021-11-10 14:12:51 -0300

    chore: add examples env

--- src/infra/config/database-config.ts
+++ src/infra/config/database-config.ts
@@ -14,8 +14,8 @@ export const connectionOptions: ConnectionOptions = {
   cache: {
     type: 'redis',
     options: {
-      host: 'localhost',
-      port: 6379
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT
     },
     duration: 20000
   },
